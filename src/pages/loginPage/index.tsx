dfaa6c461f5c8615654ad128549de7f73ba3304f
javlo <javlon@gmail.com>
2022-08-31 13:47:09 +0500

    process

--- src/pages/loginPage/index.tsx
+++ src/pages/loginPage/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Root} from "./style";
 import imgLogo from '/logo.png'
-import {Typography} from "@mui/material";
+import magicImg from '/img-magic.png'
+import avsText from '/avs-text.png'
+import {Box, Button, Checkbox, TextField, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export const LoginPage = () => {
 
+    const navigate = useNavigate();
+
+    const [loginData, setLoginData] = useState({username: '', userEmail: ''});
+    const [notValidate, setNotValidate] = useState(false)
+
+    const handleLogin = ({target: {name, value}}: any) => {
+        setLoginData({
+            ...loginData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        if (loginData.username === '' && loginData.userEmail === '') {
+            setNotValidate(true)
+        } else {
+            navigate('/main')
+        }
+    };
+
+    useEffect(() => {
+        (loginData.username !== '' && loginData.userEmail !== '') && setNotValidate(false)
+    }, [loginData])
 
     return (
         <Root>
@@ -20,13 +47,75 @@ export const LoginPage = () => {
                     <Typography className='sub-title'>
                         Please, fill in the form and get a free license key for your PC
                     </Typography>
-                    <form>
-
-                    </form>
+                    <Box
+                        component="form"
+                        noValidate
+                        autoComplete="off"
+                        onSubmit={handleSubmit}
+                    >
+                        <div className='input-wrapper'>
+                            <Typography className='input-label'>Name</Typography>
+                            <TextField
+                                error={notValidate}
+                                className='input-style'
+                                type="text"
+                                name="username"
+                                id="username"
+                                value={loginData.username}
+                                onChange={handleLogin}
+                                helperText={notValidate ? "Please enter your valid email address" : ''}
+                            />
+                        </div>
+                        <div className='input-wrapper'>
+                            <Typography className='input-label'>Email address</Typography>
+                            <TextField
+                                error={notValidate}
+                                className='input-style'
+                                type="text"
+                                name="userEmail"
+                                id="userEmail"
+                                value={loginData.userEmail}
+                                onChange={handleLogin}
+                                helperText={notValidate ? "Please enter your valid email address" : ''}
+                            />
+                        </div>
+                        <div className='checkbox-wrapper'>
+                            <div className='checkbox-agree'>
+                                <Checkbox
+                                    sx={{
+                                        width: '20px',
+                                        height: '20px',
+                                        color: '#FE9235',
+                                        '&.Mui-checked': {
+                                            color: '#FE9235',
+                                        },
+                                    }}
+                                />
+                                <Typography>
+                                    By providing my personal data I agree to be informed about AVS4YOU software deals
+                                    via
+                                    email.
+                                </Typography>
+                            </div>
+                            {notValidate && (
+                                <Typography className='error-text-license'>
+                                    Please agree to receive your free license key.
+                                </Typography>
+                            )}
+                        </div>
+                        <Button type='submit' variant='contained' className='card-btn'>
+                            Get Free License
+                        </Button>
+                    </Box>
                 </div>
             </div>
             <div className='right-side'>
-
+                <div className='avs-text-img'>
+                    <img src={avsText} alt="mag"/>
+                </div>
+                <div className='magic-img'>
+                    <img src={magicImg} alt="mag"/>
+                </div>
             </div>
         </Root>
     );
--- src/pages/loginPage/style.ts
+++ src/pages/loginPage/style.ts
@@ -1,7 +1,8 @@
 import {styled} from '@mui/styles';
 
 export const Root = styled('div')({
-
+    display: 'flex',
+    justifyContent: 'space-between',
     '& div.left-side': {
         '& div.left-line': {
             position: 'fixed',
@@ -14,7 +15,8 @@ export const Root = styled('div')({
             margin: '46px 0 0 38px'
         },
         '& div.login-card': {
-            margin: '130px 0 0 26px',
+            margin: '130px 0 110px 26px',
+            maxWidth: '500px',
             '& h3': {
                 fontWeight: '700',
                 fontSize: '37px',
@@ -22,8 +24,88 @@ export const Root = styled('div')({
             },
             '& p.sub-title': {
                 fontWeight: '600',
-                fontSize: '17px'
+                fontSize: '17px',
+                marginBottom: '30px'
+            },
+            '& .input-wrapper': {
+                '& p.input-label': {
+                    color: '#241C15',
+                    fontSize: '15px',
+                    fontWeight: '700',
+                    marginBottom: '10px'
+                },
+                '& .input-style': {
+                    width: '100%',
+                    background: '#fff',
+                    marginBottom: '30px',
+                    '& input': {
+                        padding: '11.5px 14px',
+                    },
+                    '& .MuiOutlinedInput-root': {
+                        '&:hover fieldset': {
+                            border: '1px solid #FE9235'
+                        },
+                        '&.Mui-focused fieldset': {
+                            border: '1px solid #FE9235',
+                            boxShadow: '0px 0px 0px 4px rgba(254, 146, 53, 0.2)'
+                        },
+                    },
+                },
+                '& p.MuiFormHelperText-root': {
+                    fontSize: '14px',
+                    fontWeight: '400',
+                    marginRight: 0,
+                    marginLeft: 0,
+                    color: '#F95847'
+
+                }
+            },
+            '& div.checkbox-wrapper': {
+                marginBottom: '35px',
+                '& div.checkbox-agree': {
+                    display: 'flex',
+                    marginBottom: '10px',
+                    '& span': {
+                        marginTop: '3px'
+                    },
+                    '& p': {
+                        marginLeft: '14px',
+                        fontWeight: '400',
+                        fontSize: '14px'
+                    }
+                },
+                '& p.error-text-license': {
+                    marginLeft: '14px',
+                    fontWeight: '400',
+                    fontSize: '14px',
+                    color: '#F95847'
+                }
+            },
+            '& .card-btn': {
+                width: '100%',
+                background: '#FE9235',
+                padding: '13px 16px',
+                '&:hover': {
+                    background: '#FFAB62'
+                }
             }
+        }
+    },
+    '& div.right-side': {
+        position: 'relative',
+        width: '50%',
+        height: 'auto',
+        background: `#FEE9E7 url('/dots_26.png')`,
+        '& div.avs-text-img': {
+          marginTop: '125px',
+          textAlign: 'center'
+        },
+        '& div.magic-img': {
+           '& img': {
+               position: 'absolute',
+               maxWidth: '100%',
+               bottom: 0
+           }
         }
     }
 });
